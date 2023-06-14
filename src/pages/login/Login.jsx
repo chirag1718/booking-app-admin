6e4040a18e92bb4e52fe354adff0b2a10ba9a024
Chirag <chiragsonar1718@gmail.com>
2023-06-14 13:27:19 +0530

    feat: created a login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,68 @@
-import "./login.scss"
-
+import "./login.scss";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const { loading, error, dispatch } = useContext(AuthContext);
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        navigate("/");
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "You're not allowed" },
+        });
+      }
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+  };
 
-export default Login
+  return (
+    <>
+      {/* <Navbar /> */}
+      <div className="login">
+        <div className="lContainer">
+          <input
+            className="lInput"
+            type="text"
+            placeholder="Username"
+            id="username"
+            onChange={handleChange}
+          />
+          <input
+            className="lInput"
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={handleChange}
+          />
+          <button disabled={loading} onClick={handleLogin} className="lButton">
+            Login
+          </button>
+          {error && (
+            <span style={{ textAlign: "center", color: "red" }}>
+              {error.message}
+            </span>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
